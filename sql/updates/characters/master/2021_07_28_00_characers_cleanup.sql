7dde1d70305374f94f0dc2ba2303a80912abaab8
MaxtorCoder <warsongkiller.s8@gmail.com>
2021-07-28 17:08:59 +0200

    DB: Fix SQL files in rewrite commit

--- sql/updates/characters/master/2021_07_28_00_characers_cleanup.sql
+++ sql/updates/characters/master/2021_07_28_00_characers_cleanup.sql
@@ -1,25 +1,25 @@
-DROP TABLE `blackmarket_auctions`;
+DROP TABLE IF EXISTS `blackmarket_auctions`;
 
-DROP TABLE `character_achievement`;
-DROP TABLE `character_achievement_progress`;
-DROP TABLE `character_garrison`;
-DROP TABLE `character_garrison_blueprints`;
-DROP TABLE `character_garrison_buildings`;
-DROP TABLE `character_garrison_followers`;
-DROP TABLE `character_garrison_follower_abilities;
-DROP TABLE `character_transmog_outfits`;
-DROP TABLE `character_void_storage`;
+DROP TABLE IF EXISTS `character_achievement`;
+DROP TABLE IF EXISTS `character_achievement_progress`;
+DROP TABLE IF EXISTS `character_garrison`;
+DROP TABLE IF EXISTS `character_garrison_blueprints`;
+DROP TABLE IF EXISTS `character_garrison_buildings`;
+DROP TABLE IF EXISTS`character_garrison_followers`;
+DROP TABLE IF EXISTS `character_garrison_follower_abilities`;
+DROP TABLE IF EXISTS `character_transmog_outfits`;
+DROP TABLE IF EXISTS `character_void_storage`;
 
-DROP TABLE `guild_achievement`;
-DROP TABLE `guild_achievement_progress`;
-DROP TABLE `guild_finder_applicant`;
+DROP TABLE IF EXISTS `guild_achievement`;
+DROP TABLE IF EXISTS `guild_achievement_progress`;
+DROP TABLE IF EXISTS `guild_finder_applicant`;
 
-DROP TABLE `item_instance_azerite`;
-DROP TABLE `item_instance_azerite_milestone_power`;
-DROP TABLE `item_instance_azerite_unlocked_essence`;
-DROP TABLE `item_instance_azerite_empowered`;
-DROP TABLE `item_instance_artifact`;
-DROP TABLE `item_instance_artifact_powers`;
-DROP TABLE `item_instance_transmog`;
+DROP TABLE IF EXISTS `item_instance_azerite`;
+DROP TABLE IF EXISTS `item_instance_azerite_milestone_power`;
+DROP TABLE IF EXISTS `item_instance_azerite_unlocked_essence`;
+DROP TABLE IF EXISTS `item_instance_azerite_empowered`;
+DROP TABLE IF EXISTS `item_instance_artifact`;
+DROP TABLE IF EXISTS `item_instance_artifact_powers`;
+DROP TABLE IF EXISTS `item_instance_transmog`;
 
-DROP TABLE `instance_scenario_progress`;
+DROP TABLE IF EXISTS `instance_scenario_progress`;
--- sql/updates/world/master/2021_07_26_00_world_creature_npcflags.sql
+++ sql/updates/world/master/2021_07_26_00_world_creature_npcflags.sql
@@ -2,4 +2,4 @@ ALTER TABLE `creature_template` ADD `npcflags2` INT(10) AFTER `npcflag`;
 
 UPDATE `creature_template` SET `npcflags2` = (`npcflag` >> 32) & 0xFFFFFFFF;
 
-ALTER TABLE `creature_template` MODIFY `npcflag` `npcflags` INT(10) AFTER `faction`;
+ALTER TABLE `creature_template` CHANGE `npcflag` `npcflags` INT(10) AFTER `faction`;
